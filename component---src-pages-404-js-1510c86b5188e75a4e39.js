1b439bcd5a3e0b729b9b9e84f93f24cf43a29520
Juan Esteban Jaramillo <thedivineplay@gmail.com>
2018-11-11 14:06:20 -0500

    Updates

---
+++ component---src-pages-404-js-1510c86b5188e75a4e39.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{296:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a);t.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-1510c86b5188e75a4e39.js.map
